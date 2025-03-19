44f78aa421109af0cc1bcb15aad00023438b1a4e
Christopher Okafor <chrisoxygen1234@gmail.com>
2025-03-19 10:39:00 +0100

    fix corsHeaders to remove wildcard

--- utils/cors.ts
+++ utils/cors.ts
@@ -1,5 +1,7 @@
+const extensionId = process.env.NEXT_PUBLIC_CHROME_EXTENSION_ID;
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": `chrome-extension://${extensionId}`,
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Credentials": "true",
 };
